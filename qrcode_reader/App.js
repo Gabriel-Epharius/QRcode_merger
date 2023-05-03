8fbf63fc681a6aac09b944b1053d8d0396eb98b9
Gabriel-Mac <gabrielmoraes@usp.br>
2023-05-03 17:44:01 -0300

    permissions modified & missingDimension added to .gradle

--- qrcode_reader/App.js
+++ qrcode_reader/App.js
@@ -8,8 +8,6 @@
 
 import React from 'react';
 import type {Node} from 'react';
-
-
 import {
   SafeAreaView,
   ScrollView,
@@ -72,7 +70,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step One!">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
