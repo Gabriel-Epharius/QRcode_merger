76b5756147252f47b58e7d2715d5a9f5fd888417
Gabriel-Mac <gabrielmoraes@usp.br>
2023-05-03 18:07:58 -0300

    react-native-camera not crashing but not working too

--- qrcode_reader/App.js
+++ qrcode_reader/App.js
@@ -6,8 +6,9 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {PureComponent} from 'react';
 import type {Node} from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -25,6 +26,9 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import { RNCamera, FaceDetector } from 'react-native-camera';
+import { AppRegistry } from 'react-native';
 
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -90,6 +94,47 @@ const App: () => Node = () => {
   );
 };
 
+class ExampleApp extends PureComponent {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+      </View>
+    );
+  }
+
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+}
+
+
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
@@ -110,3 +155,5 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+AppRegistry.registerComponent('App', () => ExampleApp);
